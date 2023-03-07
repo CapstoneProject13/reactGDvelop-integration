c03c7ebd9055bc7ac9dc9dc97be0d571dccadfbd
muy5310 <muy5310@naver.com>
2023-03-07 13:30:51 +0900

    feat.integration

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        title="GDevelop Game"
+        src="./game/index.html"
+        width="100%"
+        height="800px"
+        frameborder="0"
+      />
+      <button>hi</button>
+    </div>
+  );
+}
+
+export default App;
